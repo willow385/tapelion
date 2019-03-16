6c2f81bbc1c91da7024ed011ca75514147412999
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-03-16 13:29:21 -0700

    Update tapewrite.cpp

--- tapewrite.cpp
+++ tapewrite.cpp
@@ -1,7 +1,21 @@
-// tapewrite.cpp - a C++ program for writing information on cassette tapes.
-// Copyright (C) Dante Falzone 2019 under the terms of the GNU General Public License 3.0 as
-// released by the Free Software Foundation.
+/*
+    tapewrite.cpp - a C++ program for writing information on cassette tapes.
+    Part of Tapelion.
+    Copyright (C) 2019  Dante James Falzone
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <iostream>
 #include <cstring>
 
