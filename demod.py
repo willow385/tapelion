15c835c99f58cdd6321b3293fb6ed8400b252df3
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-03-16 13:26:45 -0700

    Update demod.py

--- demod.py
+++ demod.py
@@ -1,3 +1,20 @@
+"""
+    demod.py - a program for reading data from casette tapes. Part of Tapelion.
+    Copyright (C) 2019  Dante James Falzone
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 import sys
 from aubio import source, pitch
 import numpy as np
