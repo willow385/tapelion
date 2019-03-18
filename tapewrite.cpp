65c52228788101b8a6e2e36ea7a3b277d0328f49
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-03-18 15:56:29 -0500

    Update tapewrite.cpp

--- tapewrite.cpp
+++ tapewrite.cpp
@@ -1,7 +1,6 @@
 /*
     tapewrite.cpp - a C++ program for writing information on cassette tapes.
-    Part of Tapelion.
-    Copyright (C) 2019  Dante James Falzone
+    Copyright (C) 2019 Dante James Falzone
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,7 +13,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 #include <iostream>
 #include <cstring>
@@ -64,7 +63,8 @@ void tone(int x) {
 }
 
 
-// Takes 0.5 seconds to play each character. A 3000 Hz tone is used to signal the end of a byte.
+// Takes 0.5 seconds to play most characters. After three tones for the ascii numbers,
+// a 3000 Hz "NULL" tone is used to separate characters.
 void asc(int a, int b, int c) {
     tone(a);
     tone(b);
@@ -175,27 +175,111 @@ void to_bin(char x) {
         asc(0, 8, 9);
     } else if (x == 'Z') {
         asc(0, 9, 0);
+    /* To save complexity, the codes for lowercase
+       characters are not used. Instead, a special
+       character (code 980) is used to indicate that
+       the next character is lowercase. */
+    } else if (x == 'a') {
+        asc(9, 8, 0);
+        asc(0, 6, 5);
+    } else if (x == 'b') {
+        asc(9, 8, 0);
+        asc(0, 6, 6);
+    } else if (x == 'c') {
+        asc(9, 8, 0);
+        asc(0, 6, 7);
+    } else if (x == 'd') {
+        asc(9, 8, 0);
+        asc(0, 6, 8);
+    } else if (x == 'e') {
+        asc(9, 8, 0);
+        asc(0, 6, 9);
+    } else if (x == 'f') {
+        asc(9, 8, 0);
+        asc(0, 7, 0);
+    } else if (x == 'g') {
+        asc(9, 8, 0);
+        asc(0, 7, 1);
+    } else if (x == 'h') {
+        asc(9, 8, 0);
+        asc(0, 7, 2);
+    } else if (x == 'i') {
+        asc(9, 8, 0);
+        asc(0, 7, 3);
+    } else if (x == 'j') {
+        asc(9, 8, 0);
+        asc(0, 7, 4);
+    } else if (x == 'k') {
+        asc(9, 8, 0);
+        asc(0, 7, 5);
+    } else if (x == 'l') {
+        asc(9, 8, 0);
+        asc(0, 7, 6);
+    } else if (x == 'm') {
+        asc(9, 8, 0);
+        asc(0, 7, 7);
+    } else if (x == 'n') {
+        asc(9, 8, 0);
+        asc(0, 7, 8);
+    } else if (x == 'o') {
+        asc(9, 8, 0);
+        asc(0, 7, 9);
+    } else if (x == 'p') {
+        asc(9, 8, 0);
+        asc(0, 8, 0);
+    } else if (x == 'q') {
+        asc(9, 8, 0);
+        asc(0, 8, 1);
+    } else if (x == 'r') {
+        asc(9, 8, 0);
+        asc(0, 8, 2);
+    } else if (x == 's') {
+        asc(9, 8, 0);
+        asc(0, 8, 3);
+    } else if (x == 't') {
+        asc(9, 8, 0);
+        asc(0, 8, 4);
+    } else if (x == 'u') {
+        asc(9, 8, 0);
+        asc(0, 8, 5);
+    } else if (x == 'v') {
+        asc(9, 8, 0);
+        asc(0, 8, 6);
+    } else if (x == 'w') {
+        asc(9, 8, 0);
+        asc(0, 8, 7);
+    } else if (x == 'x') {
+        asc(9, 8, 0);
+        asc(0, 8, 8);
+    } else if (x == 'y') {
+        asc(9, 8, 0);
+        asc(0, 8, 9);
+    } else if (x == 'z') {
+        asc(9, 8, 0);
+        asc(0, 9, 0);
 
     } else {
         cout << "FATAL ERROR invalid/unrecognized char" << endl;
+        exit(EXIT_FAILURE);
     }
 }
 
 
 int main(void) {
-    cout << "Type in the string that you wish to translate and press ENTER." << endl;
+    cout << "Type in the string that you wish to write to the medium and press ENTER." << endl;
     string input;
     getline(cin, input);
-    system("play -n synth 1 sin 3250"); // Demarcate start of file
+    system("play -n synth 1 sin 3250"); // Demaracate start of file
+//    float time = 2.5;
     for (int i = 0; i < strlen(input.c_str()); i++) { // Convert the file into audio (stdin for the moment)
         to_bin(input.c_str()[i]);
         cout << input.c_str()[i];
+//        time += 0.5;
     }
+    asc(0, 1, 2);
     system("play -n synth 1 sin 3250"); // Demarcate end of file
     cout << "\nFinished.\n";
-    cout << "Loading time: ";
-    float time = 2 + (0.5 * strlen(input.c_str()));
-    cout << time << " seconds\n";
+//    cout << "Total playing time: " << time << " seconds\n";
     cout << "Amount of data: ";
     float filesize = strlen(input.c_str());
     cout << filesize << " bytes (approx. " << (filesize / 1024) << " KB)\n";
